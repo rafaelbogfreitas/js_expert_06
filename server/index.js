24c91390c8480075220ce38a335f49cb25c4a63e
Rafael Freitas <rafaelborges1985@hotmail.com>
2022-03-20 10:32:37 -0300

    Finish audio multiplexing

--- server/index.js
+++ server/index.js
@@ -3,3 +3,6 @@ import server from "./server.js";
 import { logger } from "./util.js";
 
 server.listen(config.port).on('listening', () => logger.info(`Server running at port: ${config.port}`));
+
+process.on("uncaughtException", (error) => logger.error(`uncaughtException: ${error.stack || error}`))
+process.on("unhandledRejection", (error) => logger.error(`unHandledRejection: ${error.stack || error}`));
