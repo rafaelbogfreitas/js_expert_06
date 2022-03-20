1115275e93b167892302d2e2013b2ae404b4ab19
Rafael Freitas <rafaelborges1985@hotmail.com>
2022-03-19 23:41:23 -0300

    Implement stop stream logic

--- public/controller/js/view.js
+++ public/controller/js/view.js
@@ -2,6 +2,8 @@ export default class View {
   constructor() {
     this.btnStart = document.getElementById("start");
     this.btnStop = document.getElementById("stop");
+    this.buttons = () => Array.from(document.querySelectorAll("button"));
+    this.ignoreButton = new Set(['unassigned']);
 
     async function onBtnClick() {};
     this.onBtnClick = onBtnClick;
@@ -28,9 +30,51 @@ export default class View {
     this.onBtnClick = fn;
   }
 
+  onStopBtn({ srcElement: { innerText }}) {
+    this.toggleBtnStart(false);
+    this.changeCommandButtonsVisibility(true);
+
+    this.onBtnClick(innerText);
+  }
+
   async onStartClicked({ srcElement: { innerText }}) {
     const btnText = innerText;
 
     await this.onBtnClick(btnText);
+    this.toggleBtnStart();
+    this.changeCommandButtonsVisibility(false);
+
+    this.buttons()
+      .filter(btn => this.isUnassignedButton(btn))
+      .forEach(btn => this.setUpBtnAction(btn));
+  }
+
+  setUpBtnAction(btn) {
+    const text = btn.innerText.toLowerCase();
+
+    if(text.includes("start")) {
+      return;
+    }
+
+    if(text.includes("stop")) {
+      btn.onclick = this.onStopBtn.bind(this);
+    }
+  }
+
+  isUnassignedButton(btn) {
+    const classes = Array.from(btn.classList);
+
+    return !(!!classes.find(item => this.ignoreButton.has(item)));
+  }
+
+  toggleBtnStart(active = true) {
+    if(active) {
+      this.btnStart.classList.add('hidden');
+      this.btnStop.classList.remove('hidden');
+      return;
+    }
+    
+    this.btnStart.classList.remove('hidden');
+    this.btnStop.classList.add('hidden');
   }
 }
